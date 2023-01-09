232adda43dad2e61999b9c888add08b53ba1697f
AimanHossain <112019059+AimanHossain@users.noreply.github.com>
2023-01-09 10:54:45 +0400

    Add files via upload

---
+++ Exercise 2.py
@@ -0,0 +1,3 @@
+msg = "“Friendship is the hardest thing in the world to explain. It’s not something you learn in school. But if you haven’t learned the meaning of friendship, you really haven’t learned anything.” — Muhammad Ali" # assigning a message to the variable
+
+print(msg) # prints the message
